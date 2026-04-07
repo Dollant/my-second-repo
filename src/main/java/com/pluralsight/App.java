343a7b1df80faecb0f154deb2a37a929e8ad4d85
Dollant <ariawilliams2425@gmail.com>
2026-04-07 11:07:05 -0400

    Added another print saying "Hello, how are you?" & Added another print saying "Final Push"

--- src/main/java/com/pluralsight/App.java
+++ src/main/java/com/pluralsight/App.java
@@ -4,5 +4,7 @@ public class App {
     public static void main(String[] args) {
         System.out.println("A cute little message");
         System.out.println("One more");
+        System.out.println("Hello, how are you?");
+        System.out.println("Final Push!");
     }
 }
